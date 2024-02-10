c60f73f2c953869d86290ee8ad4594119957348f
Maciej Szuwarowski <m9szuwarowski@interia.pl>
2024-02-10 04:14:21 +0100

    added new method that returns all languages

--- src/main/java/org/example/LangRepository.java
+++ src/main/java/org/example/LangRepository.java
@@ -2,9 +2,22 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 
+import java.util.List;
 import java.util.Optional;
 
 public class LangRepository {
+
+    List<Lang> findAll() {
+        EntityManager entityManager = JpaManager.getEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            var result = entityManager.createQuery("from Lang", Lang.class).getResultList();
+            entityManager.getTransaction().commit();
+            return result;
+        } finally {
+            JpaManager.closeEntityManager(entityManager);
+        }
+    }
 
     void createNewLanguage(Integer id, String welcomeMsg, String code) {
         EntityManager entityManager = JpaManager.getEntityManager();
